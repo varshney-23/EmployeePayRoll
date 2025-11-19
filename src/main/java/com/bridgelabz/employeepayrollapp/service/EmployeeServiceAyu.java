b4c2529a32522d7a97bf0980643a0092d9dcbba0
Ayushi Varshney <121843249+varshney-23@users.noreply.github.com>
2025-11-19 22:29:36 +0530

    Update and rename AmEmployeeService.java to EmployeeServiceAyu.java

--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceAyu.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceAyu.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.employeepayrollapp.service;
 
-import com.bridgelabz.employeepayrollapp.Interface.IEmployee;
+import com.bridgelabz.employeepayrollapp.Interface.IEmployeeAyu;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class AmEmployeeService implements IEmployee {
+public class EmployeeServiceAyu implements IEmployeeAyu {
 
 
     @Autowired
