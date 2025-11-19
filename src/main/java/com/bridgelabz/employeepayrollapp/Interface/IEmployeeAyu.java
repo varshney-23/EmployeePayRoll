d29b17ef13f3a72711e78d2c8020548b4f3d9d48
Ayushi Varshney <121843249+varshney-23@users.noreply.github.com>
2025-11-19 22:28:40 +0530

    Update and rename IEmployee.java to IEmployeeAyu.java

--- src/main/java/com/bridgelabz/employeepayrollapp/Interface/IEmployeeAyu.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/Interface/IEmployeeAyu.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public interface IEmployee {
+public interface IEmployeeAyu {
     List<Employee> getAllEmployees();
     Employee getEmployeeById(int id);
     Employee addEmployee(Employee employee);
